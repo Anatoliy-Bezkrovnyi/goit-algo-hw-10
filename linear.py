d531aa20323735130199bbf18260032387bcf0ee
Anatoliy-Bezkrovnyi <bezkrovnyi1@gmail.com>
2024-02-05 18:48:17 +0200

    hw_10_adjustments

--- linear.py
+++ linear.py
@@ -2,12 +2,14 @@ import pulp
 
 beverage = pulp.LpProblem("Producing drinks", pulp.LpMaximize)  # Максимізація
 
-lemonade = pulp.LpVariable("lemonade", lowBound=0, upBound=30, cat=pulp.LpInteger)
-juice = pulp.LpVariable("juice", lowBound=0, upBound=20, cat=pulp.LpInteger)
+lemonade = pulp.LpVariable("lemonade", lowBound=0, upBound=None, cat=pulp.LpInteger)
+juice = pulp.LpVariable("juice", lowBound=0, upBound=None, cat=pulp.LpInteger)
 
 beverage += lemonade + juice, "Max_quantity"
 
 beverage += 2 * lemonade + juice <= 100, "Water"
+beverage += 2 * juice <= 40, "Mashed fruits"
+beverage += lemonade <= 30, "Lemon Juice"
 
 beverage.solve()
 print(f"Status: {pulp.LpStatus[beverage.status]}")
